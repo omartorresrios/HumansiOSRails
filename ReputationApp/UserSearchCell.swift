6c32279cb2a7f795123641234d3687c0cc3ef050
Omar Torres <torresomar44@gmail.com>
2017-07-31 00:39:25 -0500

    Improvements in UsearSearchController and UserProfileController

--- ReputationApp/UserSearchCell.swift
+++ ReputationApp/UserSearchCell.swift
@@ -13,56 +13,53 @@ class UserSearchCell: UICollectionViewCell {
     var user: User? {
         didSet {
             fullnameLabel.text = user?.fullname
-            usernameLabel.text = user?.username
             
+            guard let profileImageUrl = user?.profileImageUrl else { return }
             
-//            guard let profileImageUrl = user?.avatarUrl else { return }
-            
-//            profileImageView.loadImage(urlString: profileImageUrl)
+            profileImageView.loadImage(urlString: profileImageUrl)
         }
     }
     
     let profileImageView: CustomImageView = {
         let iv = CustomImageView()
         iv.contentMode = .scaleAspectFill
-        iv.backgroundColor = .red
         iv.clipsToBounds = true
+        iv.layer.cornerRadius = 5
         return iv
+    }()
+    
+    let labelView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        return view
     }()
     
     let fullnameLabel: UILabel = {
         let label = UILabel()
-        label.text = "Fullname"
-        label.font = UIFont.boldSystemFont(ofSize: 14)
-        return label
-    }()
-    
-    let usernameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Username"
-        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.textColor = .white
+        label.font = UIFont(name: "SFUIDisplay-Medium", size: 14)
+        label.textAlignment = .center
         return label
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-//        addSubview(profileImageView)
-        addSubview(fullnameLabel)
-        addSubview(usernameLabel)
+        // Shadow effect
+        self.layer.cornerRadius = 3.0
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.black.withAlphaComponent(0.5).cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 2)
+        self.layer.shadowOpacity = 0.8
         
-//        profileImageView.anchor(top: nil, left: leftAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 50, height: 50)
-//        profileImageView.layer.cornerRadius = 50 / 2
-//        profileImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+
+        profileImageView.addSubview(labelView)
+        labelView.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 20)
         
-        fullnameLabel.anchor(top: topAnchor, left: leftAnchor, bottom: usernameLabel.topAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 8, paddingBottom: 4, paddingRight: 0, width: 0, height: 0)
-        
-        usernameLabel.anchor(top: fullnameLabel.bottomAnchor, left: fullnameLabel.leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
-        
-        let separatorView = UIView()
-        separatorView.backgroundColor = UIColor(white: 0, alpha: 0.5)
-        addSubview(separatorView)
-        separatorView.anchor(top: nil, left: usernameLabel.leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0.5)
+        labelView.addSubview(fullnameLabel)
+        fullnameLabel.anchor(top: labelView.topAnchor, left: labelView.leftAnchor, bottom: labelView.bottomAnchor, right: labelView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
         
     }
     
